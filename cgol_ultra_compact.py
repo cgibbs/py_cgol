cc54bd278c6e5466e8646c292e71bd7c844746c8
Chance Gibbs <kcg005@shsu.edu>
2016-02-03 15:19:46 -0600

    A tiny, tiny bit more compact

--- cgol_ultra_compact.py
+++ cgol_ultra_compact.py
@@ -4,7 +4,7 @@ l.console_init_root(SW,SH,'CGOL',0)
 def cl(b,x,y):return 0 if x==0 or x==SW-1 or y==0 or y==SH-1 else 1<sn(b, x, y)<4 if b[x][y] else 3==sn(b,x,y)
 def sn(b,x,y):return sum(b[x-1][y-1:y+2]+b[x+1][y-1:y+2])+b[x][y-1]+b[x][y+1]
 def cb(b):return [[cl(b,x,y) for y in range(SH)] for x in range(SW)]
-(m,k,old_m_pos,i,b)=(l.Mouse(),l.Key(),(0,0),0,[[0 for y in range(SH)] for x in range(SW)])
+(m,k,i,b)=(l.Mouse(),l.Key(),0,[[0 for y in range(SH)] for x in range(SW)])
 while 1:
 	l.sys_check_for_event(l.EVENT_KEY_PRESS|l.EVENT_MOUSE, k, m)
 	if k.vk==l.KEY_ESCAPE:break
